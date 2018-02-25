7329465011831782f796dfa46b988ad9216688d9
kdavis012218 <35358468+kdavis012218@users.noreply.github.com>
2018-02-25 18:58:58 -0500

    exercise 16

---
+++ ConsoleApp69/Exercise 16.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp69
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i=0; i<10; i++)
+            {
+                for (int k = 0; k <= i; k++)
+                    Console.Write('*');
+                Console.WriteLine();
+            }
+        }
+    }
+}
